32feeba33f40155ad7112614ced2f2f4624b0c4f
David Avs <david@cloudfindhq.com>
2015-07-25 22:33:00 -0400

    Use Scrapy linkextractor to extract links

--- david_avs_spider.py
+++ david_avs_spider.py
@@ -1,12 +1,15 @@
-import scrapy
+from scrapy import Spider, Request
+from scrapy.linkextractors import LinkExtractor
+from scrapy.link import Link
 
 
-class DavidAvsSpider(scrapy.Spider):
+class DavidAvsSpider(Spider):
     name = 'davidavs'
     allowed_domains = ['davidavs.com']
     start_urls = ['http://davidavs.com']
     links_seen = set()
     links_fetched = set()  # May be different due to 302 redirects
+    extractor = LinkExtractor()
 
     def parse(self, response, level=1):
         # Check if this url already has been returned
@@ -16,12 +19,14 @@ class DavidAvsSpider(scrapy.Spider):
 
         # Collect links
         links = set()
-        for href in response.css('a::attr(href)'):
-            full_url = response.urljoin(href.extract())
+        # for href in response.css('a::attr(href)'):
+        #     full_url = response.urljoin(href.extract())
+        for link in self.extractor.extract_links(response):
+            full_url = response.urljoin(link.url)
             links.add(full_url)
             if full_url not in self.links_seen:
                 self.links_seen.add(full_url)
-                yield scrapy.Request(full_url, callback=lambda resp: self.parse(resp, level+1))
+                yield Request(full_url, callback=lambda resp: self.parse(resp, level+1))
 
         # Collect static objects
         imgs = set()
